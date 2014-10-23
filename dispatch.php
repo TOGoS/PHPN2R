c2966f504c30cf369a2a04f78865e4e2af8f5d36
TOGoS <togos00@gmail.com>
2014-10-23 08:58:56 -0500

    Dispatch.php now works as a router for PHP's built-in web server.

--- dispatch.php
+++ dispatch.php
@@ -7,6 +7,22 @@
 // It also demonstrates the handleRequest function,
 // which may be useful if you're using this project
 // as a library from another.
+//
+// This is designed to work either as a mod_rewritten-to script
+// or as a standalone PHP web server router script.
 
 $res = require 'setup.php';
+
+if( !isset($_SERVER['PATH_INFO']) ) {
+	// Standalone router mode
+	if( preg_match( '#^/uri-res(/[^\?]*)\??(.*)#', $_SERVER['REQUEST_URI'], $bif ) ) {
+		$_SERVER['PATH_INFO'] = $bif[1];
+		$_SERVER['QUERY_STRING'] = $bif[2];
+	} else {
+		send_error_headers("404 What");
+		echo "Don't know how to handle URL: {$_SERVER['REQUEST_URI']}\n";
+		exit;
+	}
+}
+
 Nife_Util::outputResponse($res->handleRequest($_SERVER['PATH_INFO']));
