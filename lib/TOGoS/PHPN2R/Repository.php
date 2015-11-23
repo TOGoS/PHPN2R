2f56a06066798934243b56e9d369311e94261e1c
TOGoS <togos00@gmail.com>
2015-11-23 17:46:35 -0600

    Document behavior of Repository better.

--- lib/TOGoS/PHPN2R/Repository.php
+++ lib/TOGoS/PHPN2R/Repository.php
@@ -2,9 +2,22 @@
 
 interface TOGoS_PHPN2R_Repository
 {
-	/** @return Nife_Blob */
+	/**
+	 * @return Nife_Blob, or null if the named blob wasn't found
+	 * 
+	 * Should throw an exception if anything goes wrong other than the
+	 * blob being not found for regular reasons.  What 'regular
+	 * reasons' are, however, is up to the implementation.
+	 */
 	public function getBlob( $urn );
 	
-	/** @return URN of the put stream */
+	/**
+	 * While this could technically do ~anything~ when putting a blob,
+	 * its generally expected that getBlob($urnReturnedByPutStream) will
+	 * return the thing that was stored.  If storage fails for some reason
+	 * this should throw an exception.
+	 *
+	 * @return URN of the put stream
+	 */
 	public function putStream( $stream, $sector='uploaded', $expectedUrn=null );
 }
